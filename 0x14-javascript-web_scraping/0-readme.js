7771792b76515e75f144aa144eab671bfcb5cc76
SOME <sourbar2001@gmail.com>
2023-05-12 18:01:17 +0000

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,18 @@
-#!/usr/bin/node
+/*#!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
     process.stdout.write(data);
+  }
+});*/
+#!/usr/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
+  if (contents === undefined) {
+    console.log(err);
+  } else {
+    console.log(contents);
   }
 });
