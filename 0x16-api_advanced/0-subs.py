64a9b379279f54b5660829aef80813c01e966a91
SOME Barsour Evence <Sourbar2001@gmail.com>
2023-08-09 05:49:41 +0000

    task 1 correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,7 @@
 import requests
 
 def number_of_subscribers(subreddit):
+"""If not a valid subreddit, return 0."""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
